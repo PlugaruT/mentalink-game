d334d75cf060abcf2b2eeff247479a78fcbcc49a
PlugaruT <plugaru.tudor@gmail.com>
2016-06-08 20:21:52 +0300

    unique id for users

--- client/js/index.js
+++ client/js/index.js
@@ -6,26 +6,39 @@ var options = {
     qos: 0
 };
 
+function generateID() {
+    var id = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < 10; i++) {
+        id += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return id;
+}
+
+var id = generateID();
 var username_set = false;
 var client = mqtt.connect(options);
-
 client.on('connect', function() {
     document.getElementById("status-icon").classList.remove('label-danger');
     document.getElementById("status-icon").classList.add('label-success');
     client.subscribe('/game');
     client.subscribe('/user/login');
+    client.subscribe('/user/' + id);
 });
 
+
 function sendName() {
-    var info = '{ "user_name" : "' + document.getElementById('nickname').value + '", "login": false }';
+    var info = '{ "user_name" : "' + document.getElementById('nickname').value + '", "login": false, "token": "' + id + '" }';
+    console.log(info)
     client.publish('/user/connect', info);
+
 }
 
 
 client.on('message', function(topic, message) {
     if (topic == "/game") {
         console.log(message.toString());
-    } else if (topic == '/user/login') {
+    } else if (topic == '/user/' + id) {
         obj = JSON.parse(message);
         console.log(obj);
         if (obj.login) {
